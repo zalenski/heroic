df058780fab3b5b37c8cd84e85a70ef854c26d97
John-John Tedro <udoprog@tedro.se>
2016-09-19 07:38:07 +0200

    [parser] use visitor when parsing

--- heroic-core/src/main/java/com/spotify/heroic/CoreQueryManager.java
+++ heroic-core/src/main/java/com/spotify/heroic/CoreQueryManager.java
@@ -42,6 +42,7 @@ import com.spotify.heroic.common.Features;
 import com.spotify.heroic.common.OptionalLimit;
 import com.spotify.heroic.filter.Filter;
 import com.spotify.heroic.filter.TrueFilter;
+import com.spotify.heroic.grammar.AnyExpression;
 import com.spotify.heroic.grammar.DefaultScope;
 import com.spotify.heroic.grammar.Expression;
 import com.spotify.heroic.grammar.FunctionExpression;
@@ -154,8 +155,8 @@ public class CoreQueryManager implements QueryManager {
                         }
                     }));
 
-                final Optional<Aggregation> aggregation =
-                    e.getSelect().map(expr -> expr.visit(new Expression.Visitor<Aggregation>() {
+                final Aggregation aggregation =
+                    e.getSelect().visit(new Expression.Visitor<Aggregation>() {
                         @Override
                         public Aggregation visitFunction(final FunctionExpression e) {
                             return aggregations.build(e.getName(), e.getArguments(),
@@ -166,14 +167,19 @@ public class CoreQueryManager implements QueryManager {
                         public Aggregation visitString(final StringExpression e) {
                             return visitFunction(e.cast(FunctionExpression.class));
                         }
-                    }));
+
+                        @Override
+                        public Aggregation visitAny(final AnyExpression e) {
+                            return visitFunction(e.cast(FunctionExpression.class));
+                        }
+                    });
 
                 final Optional<Filter> filter = e.getFilter();
 
                 return new QueryBuilder()
                     .source(source)
                     .range(range)
-                    .aggregation(aggregation)
+                    .aggregation(Optional.of(aggregation))
                     .filter(filter);
             }
         });
